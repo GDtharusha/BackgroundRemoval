9364a705355ee450a31a2ddd6925554e7de6a79c
GDtharusha <73280481+GDtharusha@users.noreply.github.com>
2024-08-10 00:10:34 +0530

    bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -1,46 +1,37 @@
-import streamlit as st
+from flask import Flask, request, jsonify
 from rembg import remove
 from PIL import Image
 from io import BytesIO
 import base64
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
+app = Flask(__name__)
 
-st.write("## Remove background from your image")
-st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
-)
-st.sidebar.write("## Upload and download :gear:")
-
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
-
-# Download the fixed image
-def convert_image(img):
+def convert_image_to_base64(img):
     buf = BytesIO()
     img.save(buf, format="PNG")
     byte_im = buf.getvalue()
-    return byte_im
+    base64_encoded = base64.b64encode(byte_im).decode('utf-8')
+    return base64_encoded
 
+def convert_base64_to_image(base64_str):
+    img_data = base64.b64decode(base64_str)
+    img = Image.open(BytesIO(img_data))
+    return img
 
-def fix_image(upload):
-    image = Image.open(upload)
-    col1.write("Original Image :camera:")
-    col1.image(image)
+@app.route('/remove-background', methods=['POST'])
+def remove_background():
+    data = request.get_json()
 
-    fixed = remove(image)
-    col2.write("Fixed Image :wrench:")
-    col2.image(fixed)
-    st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+    if 'image' not in data:
+        return jsonify({'error': 'No image data provided'}), 400
 
+    try:
+        image = convert_base64_to_image(data['image'])
+        fixed = remove(image)
+        fixed_base64 = convert_image_to_base64(fixed)
+        return jsonify({'fixed_image': fixed_base64}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-
-if my_upload is not None:
-    if my_upload.size > MAX_FILE_SIZE:
-        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
-    else:
-        fix_image(upload=my_upload)
-else:
-    fix_image("./zebra.jpg")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
